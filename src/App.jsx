d19062a06497a117486a0ff591690919f9a5f311
jofay-zs <jofayzs19@gmail.com>
2022-02-04 15:49:12 -0500

    Adding metadata

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { General } from "./Layout/General";
 import Home from "./pages/Home";
 import Cryptocurrencies from "./pages/Cryptocurrencies";
 import Cryptodetails from "./pages/Cryptodetails";
 import Exchanges from "./pages/Exchanges";
 import News from "./pages/News";
-import { General } from "./Layout/General";
+import Mentions from "./pages/Mentions";
 import "./App.css";
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="cryptocurrencies" element={<Cryptocurrencies />}>
             <Route path=":coinid" element={<Cryptodetails />} />
           </Route>
+          <Route path="mentions" element={<Mentions />} />
         </Route>
       </Routes>
     </div>
---
+++ src/pages/Mentions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Mentions = () => {
+  return (
+    <div>
+      <a href="https://www.flaticon.com/free-icons/crypto" title="crypto icons">
+        Crypto icons created by GOWI - Flaticon
+      </a>
+    </div>
+  );
+};
+
+export default Mentions;
